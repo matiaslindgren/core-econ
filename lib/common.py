4fcd93c29ac995764bf6c2f9268bc07d099b9e37
Matias Lindgren <matias.lindgren@iki.fi>
2022-02-05 20:38:32 +0200

    improve altair chart, add interaction

--- lib/common.py
+++ lib/common.py
@@ -1,12 +1,17 @@
 import datetime
+import io
+import json
 import pathlib
 import sys
+import textwrap
 
 import bs4
 import jinja2
+import joblib
 import pandas as pd
+import requests
 
-bokeh_version = "2.4.0"
+memory = joblib.Memory(pathlib.Path(".").joinpath("cache"), verbose=0)
 
 
 def now():
@@ -27,15 +32,43 @@ def render(name, scripts, divs):
         lstrip_blocks=True,
     )
     template = env.get_template("main.j2")
-    html = template.render(bokeh_version=bokeh_version, scripts=scripts, divs=divs)
+    html = template.render(scripts=scripts, divs=divs)
     tree = bs4.BeautifulSoup(html, features="html.parser")
     return tree.prettify()
 
 
-def read_csv(filename, **kwargs):
+@memory.cache
+def read_csv(url, **kwargs):
+    if url.startswith("http"):
+        eprint(f"downloading {url}")
+        res = requests.get(url, timeout=10)
+        res.raise_for_status()
+        assert (
+            res.encoding == "utf-8"
+        ), f"unexpected encoding {res.encoding} in response from GET to {url}"
+        return pd.read_csv(io.StringIO(res.text), **kwargs)
     csv_path = pathlib.Path(".").parent.joinpath("data", filename)
     return pd.read_csv(csv_path, **kwargs)
 
 
-def eprint(*a, **kw):
-    print(*a, **dict(file=sys.stderr, **kw))
+def eprint(*a, file=sys.stderr, **kw):
+    print(*a, file=file, **kw)
+
+
+def altair_chart_to_html(chart, renderer="canvas", actions=False, **opt):
+    spec = chart.to_json()
+    opt = dict(renderer=renderer, actions=actions, **opt)
+    opt = json.dumps(opt)
+    html = f"""
+        <div id="vis"></div>
+        <script type="text/javascript">
+          var spec = {spec};
+          var opt = {opt};
+          vegaEmbed("#vis", spec, opt).then(() => {{
+              const bindings = document.querySelector("#vis.vega-embed form.vega-bindings");
+              const canvas = document.querySelector("#vis.vega-embed canvas");
+              canvas.parentNode.insertBefore(bindings, canvas);
+          }});
+        </script>
+    """
+    return textwrap.dedent(html)
--- src/inequality/main.py
+++ src/inequality/main.py
@@ -1,29 +1,17 @@
 import sys
-import textwrap
-from math import pi
 
 import pandas as pd
 import altair as alt
-from bokeh.embed import components
-from bokeh.models import (
-    ColumnDataSource,
-    BasicTicker,
-    ColorBar,
-    LinearColorMapper,
-    PrintfTickFormatter,
-)
-from bokeh.plotting import figure
-from matplotlib import cm
-from matplotlib.colors import rgb2hex
+
+alt.data_transformers.disable_max_rows()
 
 import common
 
+DATA_URL = "https://raw.githubusercontent.com/jackblun/Globalinc/master/GCIPrawdata.csv"
 
-def plot_income(data, year):
-    low, high = data["Mean Income"].min(), data["Mean Income"].max()
-    country_ord = {
-        c: i for i, c in enumerate(data.sort_values("Mean Income").Country.unique())
-    }
+
+def plot_income(data):
+    data["Inequality"] = data["Decile 10 Income"] / data["Decile 1 Income"]
     data = data.melt(
         id_vars=["Country", "Year"],
         value_vars=[f"Decile {i} Income" for i in range(1, 11)],
@@ -31,91 +19,58 @@ def plot_income(data, year):
         var_name="Decile",
     )
     data.Decile = data.Decile.str.split(expand=True)[1].astype(int)
-    data = data.set_index(["Country", "Year"])
-    data = data.xs(year, level="Year")
-    data = data.pivot(columns="Decile", values="Income")
-    countries = sorted(data.index, key=country_ord.get)
-    income_deciles = data.columns.astype(str).to_list()
-    data = pd.DataFrame(data.stack(), columns=["Income"]).reset_index()
-
-    cmap = cm.get_cmap("YlGn", len(income_deciles))
-    colors = [rgb2hex(cmap(i)) for i in range(cmap.N)]
-    mapper = LinearColorMapper(palette=colors, low=low, high=high)
-
-    scale = 2
-    font_px = 16
-    rect_px = 20
-
-    p = figure(
-        title="income",
-        x_range=income_deciles,
-        y_range=countries,
-        x_axis_location="above",
-        height=len(countries) * rect_px,
-        width=len(income_deciles) * rect_px,
-        tools="hover",
-        toolbar_location=None,
-        tooltips=[("Country", "@Country"), ("Income", "@Income (@Decile. decile)")],
-    )
-
-    common.eprint(countries, income_deciles, p.width, p.height)
-    p.grid.grid_line_color = None
-    p.axis.axis_line_color = None
-    p.axis.major_tick_line_color = None
-    p.axis.major_label_text_font_size = f"{font_px}px"
-    p.axis.major_label_standoff = 0
-
-    p.rect(
-        x="Decile",
-        y="Country",
-        width=rect_px,
-        height=rect_px,
-        source=data,
-        width_units="screen",
-        height_units="screen",
-        fill_color={"field": "Income", "transform": mapper},
-        line_color=None,
-    )
-
-    # color_bar = ColorBar(
-    #     color_mapper=mapper,
-    #     major_label_text_font_size=f"{font_px}px",
-    #     ticker=BasicTicker(desired_num_ticks=len(colors)),
-    #     formatter=PrintfTickFormatter(format="{:,d}$"),
-    #     label_standoff=6,
-    #     border_line_color=None,
-    # )
-    # p.add_layout(color_bar, "above")
-
-    return p
-
-
-def plot_income_altair(data, year):
-    data = data.melt(
-        id_vars=["Country", "Year"],
-        value_vars=[f"Decile {i} Income" for i in range(1, 11)],
-        value_name="Income",
-        var_name="Decile",
-    )
-    data.Decile = data.Decile.str.split(expand=True)[1].astype(int)
-    data = data.set_index(["Country", "Year"])
-    data = data.xs(year, level="Year")
     data = data.reset_index()
-    plot = (
+    common.eprint(data.shape)
+
+    width, height = 2, 16
+    scale = 100
+
+    slider = alt.binding_range(min=data.Year.min(), max=data.Year.max(), step=1)
+    select_year = alt.selection_single(
+        name="Year", fields=["Year"], bind=slider, init={"Year": data.Year.min()}
+    )
+    chart = (
         alt.Chart(data)
         .mark_rect()
-        .encode(x="Decile:O", y="Country:N", color="Income:Q")
-        .properties(width=600)
+        .encode(
+            alt.X(
+                "Decile",
+                type="ordinal",
+                axis=alt.Axis(title="Income decile", orient="top", labelAngle=0),
+                scale=alt.Scale(padding=0),
+            ),
+            alt.Y(
+                "Country",
+                type="nominal",
+                sort=alt.Sort(op="median", field="Income", order="descending"),
+                axis=alt.Axis(title=None),
+                scale=alt.Scale(padding=0),
+            ),
+            alt.Color(
+                "Income",
+                type="quantitative",
+                legend=alt.Legend(
+                    title="Income (USD)",
+                    gradientLength=scale * width,
+                    orient="top",
+                    direction="horizontal",
+                    titleAnchor="middle",
+                ),
+                scale=alt.Scale(
+                    scheme="yellowgreen",
+                    domainMin=data.Income.min(),
+                    domainMax=data.Income.max(),
+                ),
+            ),
+            tooltip=["Country", "Year", "Decile", "Income"],
+        )
+        .properties(width=scale * width, height=scale * height)
+        .add_selection(select_year)
+        .transform_filter(select_year)
+        .configure_axis(grid=False)
+        .configure_view(strokeWidth=0)
     )
-    html = f"""
-        <div id="vis"></div>
-        <script type="text/javascript">
-          var spec = {plot.to_json()};
-          var opt = {{"renderer": "canvas", "actions": false}};
-          vegaEmbed("#vis", spec, opt);
-        </script>
-    """
-    return textwrap.dedent(html)
+    return common.altair_chart_to_html(chart)
 
 
 def main(name):
@@ -125,21 +80,11 @@ def main(name):
         [
             "<h1>Inequality as rich-to-poor ratio by GDP ($ PPP)</h1>",
             f"<b>{common.now()}</b>",
+            "<br>",
         ]
     )
-    data = common.read_csv("GCIPrawdata.csv", header=2)
-    data["Inequality"] = data["Decile 10 Income"] / data["Decile 1 Income"]
-
-    # for year in (1980, 1990, 2014):
-    #     plot = plot_inequality(data[data.Year == year])
-    #     s, d = components(plot)
-    #     scripts.append(s)
-    #     divs.extend([f"<h2>{year}</h2>", d])
-
-    html = plot_income_altair(data, 1990)
-    # plot = plot_income(data, 1990)
-    # s, d = components(plot)
-    # scripts.append(s)
+    data = common.read_csv(DATA_URL, header=2)
+    html = plot_income(data)
     divs.append(html)
     print(common.render(name, scripts, divs))
     return data
